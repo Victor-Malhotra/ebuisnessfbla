be61c360df01ea42f63a0aa829a998b6ecec1655
Victor Malhotra <victorrmalhotra@gmail.com>
2023-02-10 14:04:54 -0700

    FIx designs on AboutPage.jsx and add AttractionDisplay.jsx

--- src/Pages/AboutPage.jsx
+++ src/Pages/AboutPage.jsx
@@ -1,17 +1,22 @@
 import AboutUs from '../Components/AboutPageAboutUs';
 import TermsAndServices from '../Components/AboutPageTermsAndPrivacy';
 import '../Styles/about.css'
+import AttractionDisplay from '../Components/AttractionDisplay';
 function AboutPage() {
   return (
-    <div className='bg-[#404959] text-[#eee] p-5 flex flex-wrap'>
-      <div className='flex flex-col lg:w-1/2 '>
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3331.237321438808!2d-111.85446648465796!3d33.39097218079098!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872ba8036d5ee495%3A0x1d473a8c4739f80c!2s902%20W%20Grove%20Ave%2C%20Mesa%2C%20AZ%2085210!5e0!3m2!1sen!2sus!4v1674856415655!5m2!1sen!2sus" width="900" height="480" style={{borderRadius: "2rem",marginTop: "1.8rem"}} className="p-5 w-full" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title='Hotel Location'></iframe>
+    <div className='bg-[#404959] text-[#eee] p-5 flex flex-wrap md:mt-6 sm: mt-11'>
+      <div className='flex flex-col w-full lg:w-1/2 '>
+        <div>
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3331.237321438808!2d-111.85446648465796!3d33.39097218079098!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x872ba8036d5ee495%3A0x1d473a8c4739f80c!2s902%20W%20Grove%20Ave%2C%20Mesa%2C%20AZ%2085210!5e0!3m2!1sen!2sus!4v1674856415655!5m2!1sen!2sus" width="900" height="480" style={{borderRadius: "2rem"}} className="p-5 w-full" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" title='Hotel Location'></iframe>
+        </div>
+        <div className='w-full'>
         <form
         action='https://formsubmit.co/8c66de1c39ebda66ec301f39f2d769b3 '
         method='POST'
-        encType='multipart/form-data'>
+          encType='multipart/form-data'
+        >
         <div className='flex -mr-1 -ml-1'>
-          <div className='mb-4 md:w-1/2 pr-4 pl-4'>
+          <div className='mb-4 w-full md: pr-4 pl-4'>
             <input
               type='text'
               className='block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded'
@@ -26,7 +31,7 @@ function AboutPage() {
         
 
         <div className='flex -mr-1 -ml-1'>
-          <div className='mb-4 md:w-1/2 pr-4 pl-4'>
+          <div className='mb-4 w-full pr-4 pl-4'>
             <input
               type='tel'
               className='block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded'
@@ -38,7 +43,7 @@ function AboutPage() {
           </div>
         </div>
         <div className='flex flex-wrap -mr-1 -ml-1'>
-          <div className='mb-4 md:w-full pr-4 pl-4'>
+          <div className='mb-4 w-full md: pr-4 pl-4'>
             <input
               type='text'
               className='block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded'
@@ -50,7 +55,7 @@ function AboutPage() {
           </div>
         </div>
         <div className='flex flex-wrap -mr-1 -ml-1 '>
-          <div className='mb-4 md:w-full pr-4 pl-4'>
+          <div className='mb-4 w-full md: pr-4 pl-4'>
             <textarea
               className='block appearance-none w-full py-1 px-2 mb-1 text-base leading-normal bg-white text-gray-800 border border-gray-200 rounded'
               rows='5'
@@ -61,7 +66,7 @@ function AboutPage() {
         </div>
         <br />
         <div className='flex flex-wrap -mr-1 -ml-1'>
-          <div className='mb-4 md:w-full pr-4 pl-4 text-center'>
+          <div className='mb-4 w-full md: pr-4 pl-4 text-center'>
             <a href='mailto:victorrmalhotra@gmail.com'>
               <button
                 type='submit'
@@ -94,10 +99,14 @@ function AboutPage() {
           <input type='hidden' name='_template' value='box' />
           <input type='text' name='_honey' style={{ display: 'none' }} />
         </div>
-      </form>
+          </form>
+          </div>
       </div>
-      <div className='text-white lg:w-1/2'>
-        Attractions nearby
+      <div className='text-white flex flex-col w-full lg:w-1/2'>
+        <div className='p-5'>
+          <h1 className='text-4xl'>Attractions Nearby</h1>
+          <AttractionDisplay name="Golfland Sunsplash" address="155 W Hampton Ave, Mesa, AZ 85210" times="Sunday 11 AM-10 PM, Monday 10 AM-10 PM,Tuesday 10 AM-10 PM,Wednesday 10 AM-10 PM,Thursday 10 AM-10 PM, Friday 10 AM-12 AM, Saturday 10 AM-12 AM" website="https://www.golfland.com/mesa/"/>
+        </div>
       </div>
     </div>
   );
